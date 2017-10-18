9a55c8be78d9281e91cfe08510182bce76be2949
mabraca <mabraca18@gmail.com>
2017-10-18 19:30:23 -0400

    CambioPruebas

---
+++ HolaMundo/BracamonteSerrano/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("Hola mundo 10-11147 y 09-10810.")
--- TareaBilletera/src/root/nested/Test_billetera.py
+++ TareaBilletera/src/root/nested/Test_billetera.py
@@ -11,9 +11,19 @@ class Test_Billetera(unittest.TestCase):
         self.assertEqual(billetera1.saldo(), 100)
         billetera1.consumir(100, "22/15/2017", "Comercio1", 20267824, 1234)
         self.assertEqual(billetera1.saldo(),0)
+    
+    def test_01(self):
+        """ Recargo mas de lo que consumo"""
+        print("Recargo mas de lo que consumo")
+        billetera1=BilleteraElectronica(1,"Maria", "Bra", 20267824, 1234)
+        billetera1.recargar(100, "20/12/2017", "Comercio1", 20267824)
+        self.assertEqual(billetera1.saldo(), 100)
+        billetera1.consumir(50, "22/15/2017", "Comercio1", 20267824, 1234)
+        self.assertEqual(billetera1.saldo(),50)
+    
     def test_1(self):
-        """ Consumo mas de lo que tengo"""
-        print("Consumir mas de lo que tengo en la cuenta")
+        """ Consumo mas de lo que recargue"""
+        print("Consumir mas de lo que recargue en la cuenta")
         billetera1=BilleteraElectronica(1,"Maria", "Bra", 20267824, 1234)
         billetera1.recargar(100, "20/12/2017", "Comercio1", 20267824)
         self.assertEqual(billetera1.saldo(), 100)
@@ -28,10 +38,10 @@ class Test_Billetera(unittest.TestCase):
         self.assertEqual(billetera1.saldo(), 100)
         billetera1.consumir(50, "22/15/2017", "Comercio1", 20267824, 123)
         self.assertEqual(billetera1.saldo(), 100)
-
+     
     def test_3(self):
         """ Consumir con cedula incorrecta"""
-        print("Consumir con clave incorrecta")
+        print("Consumir con cedula incorrecta")
         billetera1=BilleteraElectronica(1,"Maria", "Bra", 20267824, 1234)
         billetera1.recargar(100, "20/12/2017", "Comercio1", 20267824)
         self.assertEqual(billetera1.saldo(), 100)
@@ -47,6 +57,15 @@ class Test_Billetera(unittest.TestCase):
         billetera1.consumir(50, "22/15/2017", "Comercio1", 20267823, 1235)
         self.assertEqual(billetera1.saldo(), 100)
     
+    def test_41(self):
+        """ Consumir con clave correcta y cedula correcta"""
+        print("Consumir con clave correcta y cedula correcta")
+        billetera1=BilleteraElectronica(1,"Maria", "Bra", 20267824, 1234)
+        billetera1.recargar(100, "20/12/2017", "Comercio1", 20267824)
+        self.assertEqual(billetera1.saldo(), 100)
+        billetera1.consumir(50, "22/15/2017", "Comercio1", 20267824, 1234)
+        self.assertEqual(billetera1.saldo(), 50)
+
     def test_5(self):
         """ Recargar saldo negativo"""
         print("Recargar saldo negativo")
@@ -55,8 +74,15 @@ class Test_Billetera(unittest.TestCase):
         self.assertEqual(billetera1.saldo(),0)
         
     def test_6(self):
-        """ Recargar con cedula falsa"""
-        print("Recargar con cedula falsa")
+        """ Recargar con cedula incorrecta"""
+        print("Recargar con cedula incorrecta")
+        billetera1=BilleteraElectronica(1,"Maria", "Bra", 20267824, 1234)
+        billetera1.recargar(100, "20/12/2017", "Comercio1", 20267823)
+        self.assertEqual(billetera1.saldo(), 0)
+    
+    def test_61(self):
+        """ Recargar con cedula correcta"""
+        print("Recargar con cedula correcta")
         billetera1=BilleteraElectronica(1,"Maria", "Bra", 20267824, 1234)
         billetera1.recargar(100, "20/12/2017", "Comercio1", 20267824)
         self.assertEqual(billetera1.saldo(), 100)
@@ -69,6 +95,30 @@ class Test_Billetera(unittest.TestCase):
         self.assertEqual(billetera1.saldo(), 100)
         billetera1.consumir(-50, "22/15/2017", "Comercio1", 20267824, 1234)
         self.assertEqual(billetera1.saldo(), 100)
+    
+    def test_8(self):
+        """ Recargar 0"""
+        print("Recargar 0 bs")
+        billetera1=BilleteraElectronica(1,"Maria", "Bra", 20267824, 1234)
+        billetera1.recargar(0, "20/12/2017", "Comercio1", 20267824)
+        self.assertEqual(billetera1.saldo(), 0)
+    
+    def test_9(self):
+        """ Consumir 0 sin haber recargado"""
+        print("Consumir 0 bs sinb haber recargado")
+        billetera1=BilleteraElectronica(1,"Maria", "Bra", 20267824, 1234)
+        billetera1.consumir(0, "22/15/2017", "Comercio1", 20267824, 1234)
+        self.assertEqual(billetera1.saldo(), 0)
+     
+    def test_10(self):
+        """ Consumir 0 y haber recargado"""
+        print("Consumir 0 bs y haber recargado")
+        billetera1=BilleteraElectronica(1,"Maria", "Bra", 20267824, 1234)
+        billetera1.recargar(100, "20/12/2sinb017", "Comercio1", 20267824)
+        self.assertEqual(billetera1.saldo(), 100)
+        billetera1.consumir(0, "22/15/2017", "Comercio1", 20267824, 1234)
+        self.assertEqual(billetera1.saldo(), 100)
+        
         
 if __name__ == '__main__':
     unittest.main()
